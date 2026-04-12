1b230cd8ec940214a787ee4adedb0dd0130dfc16
Artem Lazunin <artlaz2435@gmail.com>
2026-04-12 10:03:49 +0300

    fix attempt

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://212.233.96.48")
+		w.Header().Set("Access-Control-Allow-Origin", "http://212.233.96.48:80")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
