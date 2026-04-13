271ef317a6528e0546a550fdf1d9bbea122b9aa7
Artem Lazunin <artlaz2435@gmail.com>
2026-04-13 21:15:23 +0300

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,7 +17,7 @@ func Load() (*Config, error) {
 	godotenv.Load()
 	return &Config{
 		Port:        getEnv("PORT", "8080"),
-		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/travel_planner?sslmode=disable"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:1111@212.233.96.48:5432/texnopark?sslmode=disable"),
 		JWTSecret:   getEnv("JWT_SECRET", "your-secret-key"),
 		FrontendURL: getEnv("FRONTEND_URL", "http://212.233.96.48"),
 	}, nil
