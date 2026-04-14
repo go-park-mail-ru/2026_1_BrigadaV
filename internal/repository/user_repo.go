cd07bcc291bef3dbb840a657ca962ac995315626
Artem Lazunin <artlaz2435@gmail.com>
2026-04-15 01:02:48 +0300

    email->login

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -19,19 +19,19 @@ func NewUserRepo(db *pgxpool.Pool) *UserRepo {
 }
 
 func (r *UserRepo) Create(ctx context.Context, user *models.User) error {
-	query := `INSERT INTO "user" (email, nickname, avatar_url, password_hash, country, city, about) 
+	query := `INSERT INTO "user" (login, nickname, avatar_url, password_hash, country, city, about) 
               VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, created_at, updated_at`
-	return r.db.QueryRow(ctx, query, user.Email, user.Nickname, user.AvatarURL, user.PasswordHash,
+	return r.db.QueryRow(ctx, query, user.Login, user.Nickname, user.AvatarURL, user.PasswordHash,
 		user.Country, user.City, user.About).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
 }
 
 func (r *UserRepo) GetByEmail(ctx context.Context, email string) (*models.User, error) {
 	log.Println("GetByEmail called with email:", email)
-	query := `SELECT id, email, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
+	query := `SELECT id, login, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
               FROM "user" WHERE email = $1`
 	var user models.User
 	err := r.db.QueryRow(ctx, query, email).Scan(
-		&user.ID, &user.Email, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
+		&user.ID, &user.Login, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
 		&user.Country, &user.City, &user.About, &user.HasReviews,
 		&user.CreatedAt, &user.UpdatedAt,
 	)
@@ -42,16 +42,16 @@ func (r *UserRepo) GetByEmail(ctx context.Context, email string) (*models.User, 
         log.Println("Database error:", err)
         return nil, err
 	}
-	log.Println("User found:", user.Email) 
+	log.Println("User found:", user.Login) 
 	return &user, err
 }
 
 func (r *UserRepo) GetByNickname(ctx context.Context, nickname string) (*models.User, error) {
-	query := `SELECT id, email, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
+	query := `SELECT id, login, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
               FROM "user" WHERE nickname = $1`
 	var user models.User
 	err := r.db.QueryRow(ctx, query, nickname).Scan(
-		&user.ID, &user.Email, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
+		&user.ID, &user.Login, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
 		&user.Country, &user.City, &user.About, &user.HasReviews,
 		&user.CreatedAt, &user.UpdatedAt,
 	)
@@ -62,11 +62,11 @@ func (r *UserRepo) GetByNickname(ctx context.Context, nickname string) (*models.
 }
 
 func (r *UserRepo) GetByID(ctx context.Context, id uint64) (*models.User, error) {
-	query := `SELECT id, email, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
+	query := `SELECT id, login, nickname, avatar_url, password_hash, country, city, about, has_reviews, created_at, updated_at 
               FROM "user" WHERE id = $1`
 	var user models.User
 	err := r.db.QueryRow(ctx, query, id).Scan(
-		&user.ID, &user.Email, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
+		&user.ID, &user.Login, &user.Nickname, &user.AvatarURL, &user.PasswordHash,
 		&user.Country, &user.City, &user.About, &user.HasReviews,
 		&user.CreatedAt, &user.UpdatedAt,
 	)
@@ -78,7 +78,7 @@ func (r *UserRepo) GetByID(ctx context.Context, id uint64) (*models.User, error)
 
 func (r *UserRepo) Update(ctx context.Context, user *models.User) error {
 	query := `UPDATE "user" SET 
-        email = $1,
+        login = $1,
         nickname = $2, 
         avatar_url = $3, 
         country = $4, 
@@ -88,6 +88,6 @@ func (r *UserRepo) Update(ctx context.Context, user *models.User) error {
         updated_at = NOW() 
     WHERE id = $8 RETURNING updated_at`
 	return r.db.QueryRow(ctx, query,
-		user.Email, user.Nickname, user.AvatarURL, user.Country, user.City, user.About, user.HasReviews, user.ID,
+		user.Login, user.Nickname, user.AvatarURL, user.Country, user.City, user.About, user.HasReviews, user.ID,
 	).Scan(&user.UpdatedAt)
 }
