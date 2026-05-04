8adac71d4a4a4f39a5f79aab51e65c84732fa9ce
Artem <artlaz2435@gmail.com>
2026-05-04 22:36:02 +0000

    fix

--- internal/dto/category.go
+++ internal/dto/category.go
@@ -3,7 +3,7 @@ package dto
 type CategoryRequest struct {
 	Name            string    `json:"name"`
 	Description     string    `json:"description"`
-	ApplicableTypes *[]string `json:"applicable_types"`
+	ApplicableTypes []string `json:"applicable_types"`
 }
 
 type CategoryResponse struct {
