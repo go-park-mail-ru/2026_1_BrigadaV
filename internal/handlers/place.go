3d86533d843cb800e146812058fccf1155927d8c
Artem Lazunin <artlaz2435@gmail.com>
2026-05-04 04:17:09 +0300

    photo url

--- internal/handlers/place.go
+++ internal/handlers/place.go
@@ -59,6 +59,7 @@ func (h *PlaceHandler) List(w http.ResponseWriter, r *http.Request) {
 			},
 			CreatedAt: p.CreatedAt,
 			UpdatedAt: p.UpdatedAt,
+			PhotoURL: p.PhotoURL,
 		}
 		if p.Category.ID != 0 {
 			pr.Category = &dto.CategoryDTO{
@@ -216,6 +217,7 @@ func (h *PlaceHandler) Search(w http.ResponseWriter, r *http.Request) {
 				},
 				CreatedAt: p.CreatedAt,
 				UpdatedAt: p.UpdatedAt,
+				PhotoURL: p.PhotoURL,
 			}
 			filtered = append(filtered, pr)
 		}
